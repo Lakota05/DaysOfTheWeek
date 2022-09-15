a139e9633c35af960a4e3af93c9eccd673f3966c
Victor Fortunato <lakotaclip@gmail.com>
2022-09-15 09:40:54 -0600

    First commit

---
+++ src/MainClass.java
@@ -0,0 +1,182 @@
+import java.util.Scanner;
+
+import java.text.NumberFormat;
+
+public class MainClass {
+
+public static void main (String[] args) {
+
+	
+	
+	Scanner scanner  = new Scanner(System.in);
+	System.out.println("Give me a number from 1 to 7: ");
+	int num = scanner.nextInt();
+	
+	
+	
+	String Sun = "Sunday";
+	String Mon = "Monday";
+	String Tue= "Tuesday";
+	String Wed = "Thursday";
+	String Fri = "Friday";
+	String Sat= "Saturday";
+	
+	int num1 = 1;
+	int num2 = 2;
+	int num3 = 3;
+	int num4 = 4;
+	int num5 = 5;
+	int num6 = 6;
+	int num7 = 7;
+	
+	
+	if (num == num1) {
+	
+	
+	System.out.println("Your day of the week is Sunday");
+	}
+	if (num == num2) {
+		
+		
+		System.out.println("Your day of the week is Monday");
+	}
+	if (num == num3) {
+		
+		
+		System.out.println("Your day of the week is Tuesday");
+	}
+	if (num == num4) {
+		
+		
+		System.out.println("Your day of the week is Wednesday");
+	}
+	if (num == num5) {
+		
+		
+		System.out.println("Your day of the week is Thursday");
+	}
+	if (num == num6) {
+		
+		
+		System.out.println("Your day of the week is Friday");
+	}
+	if (num == num7) {
+		
+		
+		System.out.println("Your day of the week is Saturday");
+	}
+	
+	
+	//int num1 = String("Sunday");
+	
+	//String Sunday = Integer.toString(num);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
+	//byte MONTHS_IN_YEARS = 12;
+	//byte PERCENT = 100;
+	
+    //Scanner scan = new Scanner(System.in);
+    
+   // System.out.println("What is your number? ");
+    	//int num = scan.nextInt();
+    	//float num = scan.nextFloat();
+    
+        	 
+    	 //if (num == 0){
+    		 //System.out.println(num); 
+    	// }
+    	 
+    	
+    	 
+    	// if (num % 2 == 0 ){
+    	    
+    	 	 //System.out.println("Even");
+    	 
+    	// }
+    	 //else {
+    	 		//System.out.println("Odd");
+    	 	 
+    	 		//if (num > 1000) {   
+    	       		// System.out.println("Your number is large!");
+    	 		//}
+    	       		// else if (num < 1) 
+    	    		// System.out.println("Your number is small!");
+    	 
+    	 
+    	 
+    	 
+    	 
+    	// }
+//}
+///}    	 
+
+  	 
+    	 
+    	
+    			
+    
+    	
+    
+ 
+    
+    	
+    	
+    	
+    
+    
+    
+    
+    
+    
+  
+    
+    //System.out.print("Principle: ");   
+    //int principle = scanner.nextInt();
+    
+    //System.out.print("Annual interest rate: ");   
+    //float annualInterest = scanner.nextFloat();
+    //float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
+ 
+    //System.out.print("Loan term in years: ");
+    //byte years = scanner.nextByte();
+    //int numberOfPayments = years * MONTHS_IN_YEARS;
+    
+   // double mortgage = principle 
+  //  		* (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) /
+   // 					(Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
+   // System.out.print(mortgage);
+   
+  //String mortgageFormatted = NumberFormatted.getCurrencyInstance).format(mortgage)
+    
+    
+    
+   
+    
+    
+    
+
+
+//private static float MONTHS_IN_YEARS() {
+	// TODO Auto-generated method stub
+	//return 0;
+
+//}
